9216d5f63cbbaf22624f892965b5f55bf589b8f4
Khaja Takreem UlLa <78692ishaq@gmail.com>
2024-07-01 14:33:37 +0530

    some change

--- Backend/Config/config.js
+++ Backend/Config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI : 'mongodb+srv://takreem:iELHmHPYPDcwKRCf@cluster0.5v8pq.mongodb.net/devwrite',
-    frontEnd : 'https://devwriting.vercel.app/'
+    frontEnd : 'https://devwriting.vercel.app'
 }
