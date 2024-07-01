eafd1e9989d4b882b70f0651a88f55e0c5234967
Khaja Takreem UlLa <78692ishaq@gmail.com>
2024-07-01 14:40:40 +0530

    cros policy changes

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,11 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: config.frontEnd,
+  origin: [
+    "http://localhost:4200",
+    config.frontEnd
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
